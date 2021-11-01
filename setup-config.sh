858c79c954fa4a691841577e524a24ff34bd5ec6
Sam Buercklin <sam@metalenz.com>
2021-10-31 22:28:37 -0400

    fixed up the setup-config script

--- setup-config.sh
+++ setup-config.sh
@@ -39,15 +39,15 @@ echo "Sourcing .bashrc"
 #
 ############################################
 
-LOAD_VIMRC="so \$HOME/.vimrc-sam"
-DIR_VIMRC="$HOME/.bashrc"
+LOAD_VIMRC="so $HOME/.vimrc-sam"
+DIR_VIMRC="$HOME/.vimrc"
 
 # If it's a clean install with no .vimrc, make one
 if [ ! -f $HOME/.vimrc ]; then
     touch $HOME/.vimrc
 fi
 
-# Symlink the .bashrc-sam file into the $HOME dir
+# Symlink the .vimrc-sam file into the $HOME dir
 if [ ! -f $HOME/.vimrc-sam ]; then
     echo "symlinking .vimrc-sam into $HOME"
     ln -s $SCRIPT_DIR/.vimrc-sam $HOME/
@@ -56,6 +56,32 @@ fi
 # If we haven't added the so command to the .vimrc, add it
 if ! grep -q "$LOAD_VIMRC" "$DIR_VIMRC"; then
     echo "Adding so command for .vimrc-sam"
-    echo "so $HOME/.vimrc-sam" >> $HOME/.vimrc
+    echo $LOAD_VIMRC >> $HOME/.vimrc
+fi
+
+############################################
+#
+# Importing my .tmux.conf: .tmux.conf-sam
+#
+############################################
+
+SOURCE_TMUX="source $HOME/.tmux.conf-sam"
+DIR_TMUX="$HOME/.tmux.conf"
+
+# If it's a clean install with no .vimrc, make one
+if [ ! -f $HOME/.tmux.conf ]; then
+    touch $HOME/.tmux.conf
+fi
+
+# Symlink the .tmux.conf-sam file into the $HOME dir
+if [ ! -f $HOME/.tmux.conf-sam ]; then
+    echo "symlinking .tmux.conf-sam into $HOME"
+    ln -s $SCRIPT_DIR/.tmux.conf-sam $HOME/
+fi
+
+# If we haven't added the so command to the .tmux.conf, add it
+if ! grep -q "$SOURCE_TMUX" "$DIR_TMUX"; then
+    echo "Adding source command for .tmux.conf-sam"
+    echo "source $HOME/.tmux.conf-sam" >> $HOME/.tmux.conf
 fi
 
