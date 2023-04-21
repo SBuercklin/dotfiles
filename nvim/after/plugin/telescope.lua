5dbeffc9ce77c9f6c23460da37e34878ca91f5b0
Sam Buercklin <sam.buercklin@gmail.com>
2023-04-21 14:34:21 -0400

    add note to install ripgrep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Requires you ripgrep to be installed
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
