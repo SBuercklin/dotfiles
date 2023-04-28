3cff4e9c84196eb4965ff198bde02d0fd6feb383
Sam Buercklin <sam.buercklin@gmail.com>
2023-04-28 09:55:59 -0400

    working terminal interaction

--- nvim/lua/sbuercklin/julia-repl.lua
+++ nvim/lua/sbuercklin/julia-repl.lua
@@ -1,7 +1,7 @@
 -- Assuming you're in a terminal in normal mode, enter terminal mode, start julia, and return
 -- to normal mode
 function startJulia(project)
-    local execute = 'using Revise, Infiltrator'
+    local execute = 'using Revise, Infiltrator, Pkg'
     local extra_args = ''
     if project ~= nil then
         extra_args = extra_args .. '--project=' .. project .. ' '
@@ -9,31 +9,24 @@ function startJulia(project)
     local start_cmd = 'julia ' .. '-i -e \"' .. execute .. '\" ' .. extra_args
 
     vim.cmd('terminal ' .. start_cmd)
-
 end
 
 -- Just go to the rightmost window, split in half, terminal in bottm, open terminal with cmd
 function createJuliaREPL(project)
-    local startwin = vim.api.nvim_get_current_win()
-    vim.cmd('wincmd 100 l') -- Go right as far as you can
+    local termbuf = vim.api.nvim_create_buf(true, true)
 
-    vim.cmd('split')
-    vim.cmd('wincmd 1 j')
-
-    local termbuf = vim.fn.bufnr()
-
-    startJulia(project)
-
-    vim.api.nvim_set_current_win(startwin)
+    vim.api.nvim_buf_call(termbuf, function() startJulia(project) end)
 
     return termbuf
 end
 
 function tabJuliaREPL()
-    if vim.t.julia_repl_buffer == nil then
+    if vim.t.julia_repl_job_id == nil then
         local repl_buf_id = createJuliaREPL(findProjectTOML())
 
-        vim.t.julia_repl_buffer = repl_buf_id
+        local repl_job_id = vim.b[repl_buf_id].terminal_job_id
+        vim.t.julia_repl_job_id = repl_job_id
+        vim.t.julia_repl_buf_id = repl_buf_id
     else
         print("Already opened Julia REPL")
     end
@@ -43,15 +36,36 @@ function findProjectTOML()
     return vim.fs.find('Project.toml',  { upward = true })[1]
 end
 
--- Keymap to start the repl
-vim.keymap.set('n', '<leader>jr', tabJuliaREPL)
+function envJuliaStatus()
+    job_id = activeJuliaREPL()
+    if job_id ~= nil then
+        vim.api.nvim_chan_send(job_id, 'Pkg.status()\n')
+    end
+end
+
+function envJuliaTest()
+    job_id = activeJuliaREPL()
+    if job_id ~= nil then
+        vim.api.nvim_chan_send(job_id, 'Pkg.test()\n')
+    end
+end
+
+function activeJuliaREPL()
+    return vim.t.julia_repl_job_id
+end
 
 vim.api.nvim_create_autocmd(
     'BufDelete', { callback = function(ev)
-        if ev['buf'] == vim.t.julia_repl_buffer then
-            vim.t.julia_repl_buffer = nil
+        if ev['buf'] == vim.t.julia_repl_buf_id then
+            vim.t.julia_repl_job_id = nil
+            vim.t.julia_repl_buf_id = nil
             print("Deleted Julia REPL from Tab")
         end
     end
     }
 )
+
+-- Keymaps for interacting with REPL
+vim.keymap.set('n', '<leader>jr', tabJuliaREPL)
+vim.keymap.set('n', '<leader>js', envJuliaStatus)
+vim.keymap.set('n', '<leader>jt', envJuliaTest)
