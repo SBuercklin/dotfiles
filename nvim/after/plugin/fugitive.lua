91a10312b13a1b8a4a61846991981bc65b25510e
Sam Buercklin <sam.buercklin@gmail.com>
2023-05-24 20:40:31 -0400

    fugitive remaps

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,13 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gb", function () vim.cmd({ cmd = 'Git', args = { 'blame' } }) end)
+vim.keymap.set("n", "<leader>gb", function() vim.cmd({ cmd = 'Git', args = { 'blame' } }) end)
+
+vim.keymap.set("n", "<leader>glg", 
+    function() 
+        vim.cmd({ cmd = 'Git',  args = { 'log --graph --oneline' } }) 
+    end
+    )
+vim.keymap.set("n", "<leader>gll", 
+    function() 
+        vim.cmd({ cmd = 'Git',  args = { 'log --graph --oneline --all' } }) 
+    end
+    )
