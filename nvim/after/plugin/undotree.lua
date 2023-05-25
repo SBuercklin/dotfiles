c0e0b234ce4ade84a8975fd4074e85169252cba3
Sam Buercklin <sam.buercklin@gmail.com>
2023-05-24 20:40:40 -0400

    undotree full width diff

--- nvim/after/plugin/undotree.lua
+++ nvim/after/plugin/undotree.lua
@@ -2,3 +2,5 @@ vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- Focuses the undo tree when it's opened
 vim.api.nvim_set_var("undotree_SetFocusWhenToggle", 1)
+
+vim.api.nvim_set_var("undotree_WindowLayout", 2)
