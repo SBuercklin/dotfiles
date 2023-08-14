dda6d53c44516c534de048066201577a55d6eed7
Sam Buercklin <sam.buercklin@gmail.com>
2023-08-14 12:38:48 -0400

    nvim-tree fixes

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -2,6 +2,6 @@ local lib = require("sbuercklin.lib")
 
 -- Settings for nvim-tree
 if lib.isModuleAvailable('nvim-tree') then
-    vim.g.loaded_netrew = 1
+    vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 end
