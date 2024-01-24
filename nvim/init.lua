4f160b6a568a18d1dc35e86922fc77bcea9ab82a
Sam Buercklin <sam.buercklin@gmail.com>
2024-01-24 18:27:15 -0500

    fix slow opening of large files by disable folding by expr

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,8 +65,8 @@ vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize"
 
 -- Code Folding, and a function below it to create the autocommands to open folds on enter
 -- ref: https://www.jmaguire.tech/posts/treesitter_folding/
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
 
 vim.api.nvim_create_autocmd(
