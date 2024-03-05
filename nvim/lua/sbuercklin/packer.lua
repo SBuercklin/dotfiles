71629c29dc4853a0b648154a1a6813b3bb4cbcee
Sam Buercklin <sam.buercklin@gmail.com>
2024-03-05 14:55:26 -0500

    telescope to 0.1.5

--- nvim/lua/sbuercklin/packer.lua
+++ nvim/lua/sbuercklin/packer.lua
@@ -91,7 +91,7 @@ use(
     {
        -- or                            , branch = '0.1.x',
        'nvim-telescope/telescope.nvim',
-       tag = '0.1.1',
+       tag = '0.1.5',
        requires = { {'nvim-lua/plenary.nvim'} },
     }
 )
