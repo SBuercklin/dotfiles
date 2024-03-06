4768b7e1d0bd042450da6c7cecb8b10e0f7e81eb
Sam Buercklin <sam.buercklin@gmail.com>
2024-03-06 13:39:06 -0500

    fixup configs

--- nvim/lua/sbuercklin/lazy.lua
+++ nvim/lua/sbuercklin/lazy.lua
@@ -68,7 +68,6 @@ local plugins = {
     {
         'numToStr/Comment.nvim',
         opts = {},
-        lazy = false,
     },
 
     -- Autopairs
@@ -147,7 +146,9 @@ local plugins = {
     {
        "kylechui/nvim-surround",
        version = "*", 
-       event = "VeryLazy",
+       config = function()
+        require("nvim-surround").setup({})
+       end
     },
 
     -- gS to split/combine contents across multiple lines
@@ -188,12 +189,163 @@ local plugins = {
         end
     },
 
+    {
+        "aserowy/tmux.nvim",
+        config = function()
+            local tmux = require("tmux")
+
+            tmux.setup(
+                {
+                    navigation = {
+                        cycle_navigation = false,
+                        enable_default_keybindings = false,
+                    },
+                    resize = {
+                        enable_default_keybindings = false,
+                    },
+                    copy_sync = {
+                        enable = false, -- I like persisting my individual nvim registers
+                        sync_clipboard = false, -- but if I did want this, I want to manage the clipboard
+                    }
+                }
+            )
+
+
+            vim.keymap.set("n", "<A-h>", tmux.move_left)
+            vim.keymap.set("n", "<A-j>", tmux.move_bottom)
+            vim.keymap.set("n", "<A-k>", tmux.move_top)
+            vim.keymap.set("n", "<A-l>", tmux.move_right)
+        end
+    },
+
+    {
+        'jpalardy/vim-slime',
+        config = function()
+            vim.g.slime_target = "tmux"
+            -- let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}
+        end
+    },
+
     -----------------------------------------
     -- LSP autocomplete, languages
     -----------------------------------------
-     {'neovim/nvim-lspconfig'},
-     {'williamboman/mason.nvim'},
-     {'williamboman/mason-lspconfig.nvim'},
+     {
+         'neovim/nvim-lspconfig',
+         dependencies = {
+             {'williamboman/mason.nvim'},
+             {'williamboman/mason-lspconfig.nvim'},
+             {'hrsh7th/cmp-nvim-lsp'},
+             {'simrat39/rust-tools.nvim'},
+         },
+         config = function (_, opts)
+            local lsp = require('lspconfig')
+            local lib = require('sbuercklin.lib')
+
+            -- require('mason').setup()
+
+            local capabilities = require('cmp_nvim_lsp').default_capabilities
+            local keymap_opts = { noremap = true, silent = true, buffer = bufnr }
+
+            -- Enable LSP logging
+            -- vim.lsp.set_log_level("debug")
+
+            -- Diagnostic shortcuts, pulled from lspconfig docs
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+            local attach_fn = function(client, bufnr)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, keymap_opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keymap_opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, keymap_opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, keymap_opts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, keymap_opts)
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, keymap_opts)
+                vim.keymap.set('n', 'F2', vim.lsp.buf.rename, keymap_opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, keymap_opts)
+                vim.keymap.set('n', '<leader>jf', vim.lsp.buf.format, keymap_opts)
+            end
+
+            local format_autocmd = function(bufnr)
+                vim.api.nvim_create_autocmd(
+                    'BufWritePre', {
+                        callback = function(ev)
+                            vim.lsp.buf.format()
+                        end,
+                        buffer = bufnr
+                    }
+                )
+            end
+
+            --------------------------------------------
+            -- Language specific configurations
+            --------------------------------------------
+
+            lsp.julials.setup(
+                {
+                    on_attach = function(client, bufnr)
+                        attach_fn(client, bufnr)
+
+                        -- This is used to determine if we want to autoformat
+                        --  kinda hacky but it works
+                        local fname = vim.api.nvim_buf_get_name(0)
+                        local dname = vim.fs.dirname(fname)
+                        local dirs = vim.fs.find('.JuliaFormatter.toml',  { upward = true, path = dname })
+                        if not(next(dirs) == nil) then
+                            format_autocmd(bufnr)
+                        end
+                    end,
+                    capabilities = capabilities()
+                }
+            )
+
+            -- Requires latexindent and latexmk for formatting and building respectively
+            lsp.texlab.setup(
+                {
+                    on_attach = function(client, bufnr)
+                        attach_fn(client, bufnr)
+                        format_autocmd(bufnr)
+                        vim.keymap.set("n", "<Leader>jm", vim.cmd["TexlabBuild"])
+                    end,
+                    capabilities = capabilities(),
+                    settings = {
+                        texlab = {
+                            build = {
+                              executable = 'latexmk',
+                              args = { '-shell-escape', '-pdf', '-interaction=nonstopmode', '-synctex=1', '%f' },
+                            }
+                        }
+                    }
+                }
+            )
+
+            -- Note that to configure the pyright env, use the venv and venvPath entries in the project config:
+            --    https://github.com/microsoft/pyright/blob/main/docs/configuration.md
+            lsp.pyright.setup(
+                {
+                    on_attach = function(client, bufnr)
+                        attach_fn(client, bufnr)
+                    end,
+                    capabilities = capabilities()
+                }
+            )
+
+            local rt = require("rust-tools")
+            rt.setup(
+                {
+                    server = {
+                        on_attach = function(client, bufnr)
+                            attach_fn(client, bufnr)
+                            
+                            format_autocmd(bufnr)
+                            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, keymap_opts)
+                            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, keymap_opts)
+                        end
+                    }
+                }
+            )
+         end
+     },
 
     -- nvim-cmp engine and autocomplete locations
      {'hrsh7th/nvim-cmp'},
@@ -208,7 +360,6 @@ local plugins = {
      {'saadparwaiz1/cmp_luasnip'},
 
      -- Language support
-     {'simrat39/rust-tools.nvim'},
      {'JuliaEditorSupport/julia-vim'},
 
     -----------------------------------------
