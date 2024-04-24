3a511c923dd5f8d627dad11de62a61b822d8c6eb
Sam Buercklin <sam.buercklin@gmail.com>
2024-04-24 16:50:29 -0400

    leader-ja works again

--- nvim/lua/sbuercklin/configs/telescope.lua
+++ nvim/lua/sbuercklin/configs/telescope.lua
@@ -1,22 +1,30 @@
 function reattach_picker(opts)
-      local tmux = require("stmux")
-      opts = opts or {}
-      local wlist = tmux.get_windowlist()
-      pickers.new(opts, {
+    local tmux = require("stmux")
+    opts = opts or {}
+    local wlist = tmux.get_windowlist()
+    local previewers = require("telescope.previewers")
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local actions = require("telescope.actions")
+    local actions_state = require("telescope.actions.state")
+    local conf = require('telescope.config').values
+    local lib = require("samlib")
+
+    pickers.new(opts, {
         prompt_title = "Select pane to attach to current tab",
         finder = finders.new_table {
             results = wlist,
         },
         sorter = conf.generic_sorter(opts),
         attach_mappings = function(prompt_bufnr, map)
-          actions.select_default:replace(
-            function()
-              actions.close(prompt_bufnr)
-              local entry = action_state.get_selected_entry()
-              tmux.attach_existing_pane(entry[1])
-            end
-          )
-          return true
+            actions.select_default:replace(
+                function()
+                    actions.close(prompt_bufnr)
+                    local entry = actions_state.get_selected_entry()
+                    tmux.attach_existing_pane(entry[1])
+                end
+            )
+            return true
         end,
         previewer = previewers.new_buffer_previewer({
             title = "Panel Contents",
@@ -25,39 +33,39 @@ function reattach_picker(opts)
                 vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, contents)
             end
         }),
-      }):find()
+    }):find()
 end
 
 return {
-       'nvim-telescope/telescope.nvim',
-       version = '0.1.5',
-       dependencies = {{'nvim-lua/plenary.nvim'}, {"stmux"}, {"samlib"}, {"snotes"}},
-       keys = {
-           { "<leader>ff", require("telescope.builtin").find_files },
-           { "<leader>fg", require("telescope.builtin").live_grep },
-           { '<leader>fa', require("telescope.builtin").lsp_workspace_symbols },
-           { '<leader>fs', require("telescope.builtin").lsp_document_symbols},
-           { '<leader>fb', require("telescope.builtin").buffers },
-           { '<leader>fh', require("telescope.builtin").help_tags },
-           { '<leader>fr', require("telescope.builtin").lsp_references, noremap = true, silent = true },
-           { '<leader>fn', function() 
-               require("telescope.builtin").live_grep({cwd = require("snotes").notes.get_note_dir()})
-           end
-           },
-           { '<leader>ja', reattach_picker }
-       },
-       opts = {
-          pickers = {
+    'nvim-telescope/telescope.nvim',
+    version = '0.1.5',
+    dependencies = { { 'nvim-lua/plenary.nvim' }, { "stmux" }, { "samlib" }, { "snotes" } },
+    keys = {
+        { "<leader>ff", require("telescope.builtin").find_files },
+        { "<leader>fg", require("telescope.builtin").live_grep },
+        { '<leader>fa', require("telescope.builtin").lsp_workspace_symbols },
+        { '<leader>fs', require("telescope.builtin").lsp_document_symbols },
+        { '<leader>fb', require("telescope.builtin").buffers },
+        { '<leader>fh', require("telescope.builtin").help_tags },
+        { '<leader>fr', require("telescope.builtin").lsp_references,       noremap = true, silent = true },
+        { '<leader>fn', function()
+            require("telescope.builtin").live_grep({ cwd = require("snotes").notes.get_note_dir() })
+        end
+        },
+        { '<leader>ja', reattach_picker }
+    },
+    opts = {
+        pickers = {
             buffers = {
-              mappings = {
-                i = {
-                  ["<C-d>"] = "delete_buffer",
-                },
-                n = {
-                  ["<C-d>"] = "delete_buffer",
+                mappings = {
+                    i = {
+                        ["<C-d>"] = "delete_buffer",
+                    },
+                    n = {
+                        ["<C-d>"] = "delete_buffer",
+                    }
                 }
-              }
             }
-          }
-       }
+        }
     }
+}
