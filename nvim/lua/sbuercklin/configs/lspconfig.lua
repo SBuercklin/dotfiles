a6bc845fd8afa309cd9c9da2c6b86fc42d7c61f5
Sam Buercklin <sam.buercklin@gmail.com>
2024-06-13 13:26:31 -0400

    remove rust-tools dep

--- nvim/lua/sbuercklin/configs/lspconfig.lua
+++ nvim/lua/sbuercklin/configs/lspconfig.lua
@@ -1,3 +1,34 @@
+local attach_fn = function(client, bufnr)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, keymap_opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keymap_opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, keymap_opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, keymap_opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, keymap_opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, keymap_opts)
+    vim.keymap.set('n', 'F2', vim.lsp.buf.rename, keymap_opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, keymap_opts)
+    vim.keymap.set('n', '<leader>jf', vim.lsp.buf.format, keymap_opts)
+            
+    -- Diagnostic shortcuts, pulled from lspconfig docs
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+end
+
+local toggle_hints = function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end
+
+local format_autocmd = function(bufnr)
+    vim.api.nvim_create_autocmd(
+        'BufWritePre', {
+            callback = function(ev)
+                vim.lsp.buf.format()
+            end,
+            buffer = bufnr
+        }
+    )
+end
+
+
 return {
      {
          'williamboman/mason.nvim',
@@ -11,7 +42,6 @@ return {
              {'williamboman/mason.nvim'},
              {'williamboman/mason-lspconfig.nvim'},
              {'hrsh7th/cmp-nvim-lsp'},
-             {'simrat39/rust-tools.nvim'},
          },
          config = function (_, opts)
             local lsp = require('lspconfig')
@@ -23,34 +53,6 @@ return {
 
             -- Enable LSP logging
             -- vim.lsp.set_log_level("debug")
-
-            -- Diagnostic shortcuts, pulled from lspconfig docs
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-            local attach_fn = function(client, bufnr)
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, keymap_opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keymap_opts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, keymap_opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, keymap_opts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, keymap_opts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, keymap_opts)
-                vim.keymap.set('n', 'F2', vim.lsp.buf.rename, keymap_opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, keymap_opts)
-                vim.keymap.set('n', '<leader>jf', vim.lsp.buf.format, keymap_opts)
-            end
-
-            local format_autocmd = function(bufnr)
-                vim.api.nvim_create_autocmd(
-                    'BufWritePre', {
-                        callback = function(ev)
-                            vim.lsp.buf.format()
-                        end,
-                        buffer = bufnr
-                    }
-                )
-            end
 
             --------------------------------------------
             -- Language specific configurations
@@ -107,19 +109,16 @@ return {
                 }
             )
 
-            -- TODO: Move away from rust-tools (or rustaceanvim) and just use the LSP directly
-            local rt = require("rust-tools")
-            rt.setup(
+            lsp.rust_analyzer.setup(
                 {
-                    server = {
-                        on_attach = function(client, bufnr)
-                            attach_fn(client, bufnr)
-                            
-                            format_autocmd(bufnr)
-                            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, keymap_opts)
-                            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, keymap_opts)
-                        end
-                    }
+                    on_attach = function(client, bufnr)
+                        attach_fn(client, bufnr)
+
+                        vim.keymap.set("n", "<Leader>a", vim.lsp.buf.code_action)
+                        vim.keymap.set("n", "<Leader>jh", toggle_hints)
+                        format_autocmd(bufnr)
+                    end,
+                    capabilities = capabilities()
                 }
             )
          end
