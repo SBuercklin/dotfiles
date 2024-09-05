92d5125785608546f824d378586925d493831fde
Sam Buercklin <sam.buercklin@gmail.com>
2024-09-05 17:08:47 -0400

    cmp formatter

--- nvim/lua/sbuercklin/configs/cmp.lua
+++ nvim/lua/sbuercklin/configs/cmp.lua
@@ -1,80 +1,78 @@
 return {
-         'hrsh7th/nvim-cmp',
-         dependencies = {
-             {'hrsh7th/cmp-cmdline'},
-             {'hrsh7th/cmp-buffer'},
-             {'hrsh7th/cmp-nvim-lua'},
-             {'hrsh7th/cmp-nvim-lsp'},
-             {'kdheepak/cmp-latex-symbols'},  -- needed for Julia autocomplete of symbol
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        { 'hrsh7th/cmp-cmdline' },
+        { 'hrsh7th/cmp-buffer' },
+        { 'hrsh7th/cmp-nvim-lua' },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'kdheepak/cmp-latex-symbols' }, -- needed for Julia autocomplete of symbol
 
-             {'rafamadriz/friendly-snippets'},
-             {'L3MON4D3/LuaSnip'},
-             {'saadparwaiz1/cmp_luasnip'},
-         },
-         config = function (_,_) 
+        { 'rafamadriz/friendly-snippets' },
+        { 'L3MON4D3/LuaSnip' },
+        { 'saadparwaiz1/cmp_luasnip' },
+    },
+    config = function(_, _)
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
+        -- Loads local snippets
+        require("luasnip.loaders.from_snipmate").load({ paths = { "~/.config/nvim/snippets/snipmate/" } })
 
-            -- Loads local snippets
-            require("luasnip.loaders.from_snipmate").load({ paths = { "~/.config/nvim/snippets/snipmate/"}})
+        local has_words_before = function()
+            unpack = unpack or table.unpack
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
 
-            local has_words_before = function()
-              unpack = unpack or table.unpack
-              local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-              return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-            end
-
-            cmp.setup({
-                snippet = {
-                  expand = function(args)
+        cmp.setup({
+            snippet = {
+                expand = function(args)
                     -- Pull snippets from luasnip
                     luasnip.lsp_expand(args.body) -- Use this to get snippets from the LSP
                     -- luasnip.expand(args.body) -- Use this to just use local snippets
-                  end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                  ['<C-Space>'] = cmp.mapping.complete(),
-                  ['<C-e>'] = cmp.mapping.abort(),
-                  ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                  ['<Tab>'] = cmp.mapping(function(fallback)
-                      if luasnip.expand_or_jumpable() then
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
-                      elseif has_words_before() then
+                    elseif has_words_before() then
                         cmp.complete()
-                      else
+                    else
                         fallback()
-                      end
-                    end, { "i", "s" }),
-                  ['<S-Tab>'] = cmp.mapping(function(fallback)
-                      if luasnip.jumpable(-1) then
+                    end
+                end, { "i", "s" }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if luasnip.jumpable(-1) then
                         luasnip.jump(-1)
-                      else
+                    else
                         fallback()
-                      end
-                    end, { "i", "s" }),
-                }),
-                sources = cmp.config.sources({
-                  { name = 'nvim_lsp' },
-                  { name = 'luasnip' }, 
-                  { name = 'nvim_lua' }, 
-                  { name = 'latex_symbols', option = { strategy = 1 } }, -- Julia autocomplete of symbols
-                }, {
-                  { name = 'buffer' },
-                }),
-              })
+                    end
+                end, { "i", "s" }),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'nvim_lua' },
+                { name = 'latex_symbols', option = { strategy = 1 } }, -- Julia autocomplete of symbols
+            }, {
+                { name = 'buffer' },
+            }),
+        })
 
-            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline(':', {
-              mapping = cmp.mapping.preset.cmdline(),
-              sources = cmp.config.sources({
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
                 { name = 'path' }
-              }, {
+            }, {
                 { name = 'cmdline' }
-              })
             })
-         end,
-         event = "InsertEnter"
-     }
+        })
+    end,
+    event = "InsertEnter"
+}
