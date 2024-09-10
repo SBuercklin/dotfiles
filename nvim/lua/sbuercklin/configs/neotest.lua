c26df841c3b7c4e1e38eede9c622695f21a46a59
Sam Buercklin <sam.buercklin@gmail.com>
2024-09-10 09:10:04 -0400

    remove CursorHold dep from neotest for updatetime fix

--- nvim/lua/sbuercklin/configs/neotest.lua
+++ nvim/lua/sbuercklin/configs/neotest.lua
@@ -3,7 +3,6 @@ return {
     dependencies = {
         "nvim-neotest/nvim-nio",
         "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
         "nvim-treesitter/nvim-treesitter",
         "sbuercklin/neotest-julials",
         "nvim-neotest/neotest-python"
